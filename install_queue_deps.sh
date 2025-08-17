75665ccd83ceaec3f74145377e3a7b4f851411c7
Adailton Saraiva <s.adailton@gmail.com>
2025-08-17 00:02:03 -0300

    correção de caminho

--- install_queue_deps.sh
+++ install_queue_deps.sh
@@ -3,10 +3,10 @@
 WORKDIR=`echo $HOME`
 PYTHON_MODULES="/usr/local/lib/python3.10/site-packages/"
 
-cd $WORKDIR
+#cd $WORKDIR
 
 #git clone https://github.com/nerds-ufes/polka.git
 
-pip3 install -r requirements.txt
+pip3 install -r ./requirements.txt
 
-sudo cp mininet_p4_queue.py $PYTHON_MODULES
+sudo cp ./mininet_p4_queue.py $PYTHON_MODULES
